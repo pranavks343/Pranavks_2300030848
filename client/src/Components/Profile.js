120b91dcaf6e5df4aff746d950bfb305170d6f34
pranavks343 <152205583+pranavks343@users.noreply.github.com>
2025-04-16 21:59:22 +0530

    Update Profile.js

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -6,10 +6,10 @@ const Profile = () => {
       <div className="bg-white p-8 rounded-2xl shadow-2xl text-center max-w-md w-full">
         <h1 className="text-3xl font-bold text-blue-700 mb-4">Profile</h1>
         <p className="text-lg text-gray-800 mb-2">
-          <strong>ID Number:</strong> 2300030848
+          <strong>ID Number:</strong> 2300031365
         </p>
         <p className="text-lg text-gray-800">
-          <strong>Name:</strong> KONDAPI SRI PRANAV
+          <strong>Name:</strong> kiliveti sihrudh amru
         </p>
       </div>
     </div>
